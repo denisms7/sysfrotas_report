e653b38cb0923b70c56a9e3ad49d095d91d91709
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2026-05-26 00:40:08 -0300

    style

--- App.py
+++ App.py
@@ -5,14 +5,14 @@ st.logo('img/logopm.png', size="large")
 
 pages = {
     "Monitoramento": [
-        st.Page("pages/Alertas.py", title="Alertas de Consumo"),
-        st.Page("pages/Alertas_Centros.py", title="Alertas por Centro de Custo"),
-        st.Page("pages/Capacidade_Tanque.py", title="Capacidade do Tanque"),
+        st.Page("pages/Alertas.py", title="Alertas de Consumo", icon="🔔"),
+        st.Page("pages/Alertas_Centros.py", title="Alertas por Centro de Custo", icon="🏢"),
+        st.Page("pages/Capacidade_Tanque.py", title="Capacidade do Tanque", icon="⛽"),
     ],
     "Requisicoes": [
-        st.Page("pages/Geral.py", title="Combustivel Utilizado"),
-        st.Page("pages/Requisicao.py", title="Litros por Requisicoes"),
-        st.Page("pages/Veiculos.py", title="Veiculos Individuais"),
+        st.Page("pages/Geral.py", title="Combustivel Utilizado", icon="📊"),
+        st.Page("pages/Requisicao.py", title="Litros por Requisicoes", icon="🔍"),
+        st.Page("pages/Veiculos.py", title="Veiculos Individuais", icon="🚗"),
     ],
 }
 
